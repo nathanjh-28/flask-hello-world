9f952fb423d240a816111aee1173b1286b294819
nathanjh-28 <nathan@nathanjharris.com>
2024-03-26 18:52:39 +0000

    fixed error with route

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from -- nathanjh-28 -- in 3308'
 
-@app.route('db_test')
+@app.route('/db_test')
 def testing():
     conn = psycopg2.connect('postgres://flaskbballdb_user:D2QJhgX5jMUAZOw883TC9tgUY5ovJJK2@dpg-co1hgf7109ks73bsbtvg-a/flaskbballdb')
     conn.close()
