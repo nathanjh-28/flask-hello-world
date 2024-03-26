addacee481d7b74ae1ec6b6d486032766916a9c0
nathanjh-28 <nathan@nathanjharris.com>
2024-03-26 18:58:20 +0000

    added db_create route and created a table

--- app.py
+++ app.py
@@ -11,3 +11,22 @@ def testing():
     conn = psycopg2.connect('postgres://flaskbballdb_user:D2QJhgX5jMUAZOw883TC9tgUY5ovJJK2@dpg-co1hgf7109ks73bsbtvg-a/flaskbballdb')
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def create():
+    conn = psycopg2.connect('postgres://flaskbballdb_user:D2QJhgX5jMUAZOw883TC9tgUY5ovJJK2@dpg-co1hgf7109ks73bsbtvg-a/flaskbballdb')
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+        );
+    """)
+    conn.commit()
+    
+    conn.close()
+
+    return "Basketball table was created with success!"
